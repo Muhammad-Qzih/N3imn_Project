2e13125a35b44d7d864322c83f12cf6a4ce90f13
muhammad qzih <muhammadghqzih@gmail.com>
2024-05-03 21:29:33 +0300

    Create Global Errors Dialog

--- lib/utils/Dialogs/dialgos.dart
+++ lib/utils/Dialogs/dialgos.dart
@@ -1,1 +1,15 @@
-class Dialogs {}
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+
+class Dialogs {
+  static void showAuthenticationErrorDialog(
+      BuildContext context, String errorMessage, String title) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.noHeader,
+      animType: AnimType.topSlide,
+      title: title,
+      desc: errorMessage,
+    ).show();
+  }
+}     
