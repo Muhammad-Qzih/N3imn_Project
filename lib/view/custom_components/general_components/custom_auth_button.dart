615c1dda483f56ddce79d5da3223d3f8070c9e51
Tubasi <mtn3213@gmail.com>
2024-05-04 20:52:40 +0300

    Create a general button

--- lib/view/custom_components/general_components/custom_auth_button.dart
+++ lib/view/custom_components/general_components/custom_auth_button.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:n3imn_project_team/themes/colors_theme.dart';
 
 class CsutomAuthButton extends StatelessWidget {
-  const CsutomAuthButton({super.key});
+  
+  final void Function()? onPressed;
+  final String buttonText;
+  final Color backgroundColor;
+  final TextStyle? textStyle;
+  const CsutomAuthButton({
+    super.key,
+   required this.buttonText,
+    this.onPressed,
+    this.backgroundColor = AppColor.PRIMARY,
+    this.textStyle = const TextStyle(
+      color: AppColor.TEXT_SECONDARY,
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+       decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(50), 
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: MaterialButton(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+        onPressed: onPressed,
+        child: Text(buttonText, style: textStyle),
+      ),
+    );
   }
 }
