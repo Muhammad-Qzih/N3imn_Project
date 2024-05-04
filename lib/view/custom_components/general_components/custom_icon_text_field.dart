38fe26715176f9da3959a4ebee523e79288043ed
muhammad qzih <muhammadghqzih@gmail.com>
2024-05-04 22:43:20 +0300

    Implement LogoTextField component

--- lib/view/custom_components/general_components/custom_icon_text_field.dart
+++ lib/view/custom_components/general_components/custom_icon_text_field.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:n3imn_project_team/themes/colors_theme.dart';
+import 'package:n3imn_project_team/themes/texts_theme.dart';
 
 class CustomIconTextField extends StatelessWidget {
-  const CustomIconTextField({super.key});
+  final String hintText;
+  final bool obsecureText;
+  final TextEditingController textEditingController;
+  final String? Function(String?)? validator;
+  final Widget icon;
+  const CustomIconTextField(
+      {super.key,
+      required this.hintText,
+      required this.obsecureText,
+      required this.textEditingController,
+      this.validator,
+      required this.icon});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextFormField(
+        validator: validator,
+        obscureText: obsecureText,
+        controller: textEditingController,
+        style: const TextStyle(color: AppColor.PRIMARY),
+        decoration: InputDecoration(
+          prefixIcon: icon,
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColor.PRIMARY)),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColor.PRIMARY)),
+          fillColor: Colors.white,
+          filled: obsecureText,
+          hintText: hintText,
+          hintStyle: AppTexts.robotoTextFieldsStyle,
+        ));
   }
 }
