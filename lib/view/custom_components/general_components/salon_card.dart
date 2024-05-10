b165cb4bf71c0ef372ab5582bb4a5729f98b301c
Tubasi <mtn3213@gmail.com>
2024-05-10 23:55:00 +0300

    enhancement SalonCard component

--- lib/view/custom_components/general_components/salon_card.dart
+++ lib/view/custom_components/general_components/salon_card.dart
@@ -7,14 +7,17 @@ class SalonCard extends StatelessWidget {
   final int rating;
   final VoidCallback onPressed;
   final String location;
+  final double? width;
+  final double? height;
 
-   SalonCard({
-    super.key,
-    required this.salonName,
-    required this.location,
-    required this.rating,
-    required this.onPressed,
-  });
+  const SalonCard(
+      {super.key,
+      required this.salonName,
+      required this.location,
+      required this.rating,
+      required this.onPressed,
+      required this.height,
+      required this.width});
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +27,31 @@ class SalonCard extends StatelessWidget {
       child: Card(
         color: const Color.fromARGB(255, 255, 255, 255),
         child: Container(
-          width: 147,
-          height: 121,
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: const BorderRadius.all(Radius.circular(9)),
+            boxShadow: [
+              BoxShadow(
+                  color: AppColor.PRIMARY.withOpacity(0.4),
+                  spreadRadius: 1.5,
+                  blurRadius: 1,
+                  offset: const Offset(3, 3)),
+            ],
+          ),
           padding: const EdgeInsets.all(10),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, 
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(salonName,
-                  style: theme.textTheme.titleSmall
+                  style: theme.textTheme.displayMedium
                       ?.copyWith(color: AppColor.TEXT_PRIMARY)),
               const Divider(color: Colors.black54),
               Row(
                 children: [
-                  const Icon(Icons.location_on_outlined, color: AppColor.PRIMARY),
+                  const Icon(Icons.location_on_outlined,
+                      color: AppColor.PRIMARY),
                   const SizedBox(width: 5),
                   Text(location,
                       style: theme.textTheme.titleSmall
