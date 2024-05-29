0670ffbe1cb32d37c791256f3186380d1baf819d
amjad67 <80590854+amjad67@users.noreply.github.com>
2024-05-29 19:10:10 +0300

    ui skltone for reservation barber managment screen

--- lib/view/barber_screens/home_navigation_screens/barber_reservations_screen.dart
+++ lib/view/barber_screens/home_navigation_screens/barber_reservations_screen.dart
@@ -11,14 +11,9 @@ class _BarberReservatonsState extends State<BarberReservatons> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          'Barber reservatons',
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [//cards reservation],
+        ));
   }
 }
